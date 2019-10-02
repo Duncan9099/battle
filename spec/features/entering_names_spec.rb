8797191a44d8137822e55c74fa9bec192f69bf2c
John Dennehy <john.dennehy@gmail.com>
2019-10-02 16:50:24 +0100

    adds features web_helper

--- spec/features/entering_names_spec.rb
+++ spec/features/entering_names_spec.rb
@@ -2,12 +2,7 @@ require "capybara/rspec"
 
 feature "Enter player names" do
   scenario "players fill out forms" do
-    visit "/"
-
-    fill_in "player1", with: "John"
-    fill_in "player2", with: "Duncan"
-    click_button "Submit"
-
+    sign_in_and_play
     expect(page).to have_text("John vs Duncan")
   end
 end
