c84d2d6ce9cc379035baf23e64c12ab8cdbcb83b
Duncan Skinner <duncanskinner94@gmail.com>
2019-10-03 13:13:14 +0100

    creating a player class

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -3,7 +3,7 @@ require 'capybara'
 feature "attack" do
   scenario "attacks player 2 and returns confirmation" do
     sign_in_and_play
-    click_button "Attack Duncan" 
+    click_button "Attack Duncan"
     expect(page).to have_text("John attacks Duncan!")
   end
 end
