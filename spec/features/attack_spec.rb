d7402f2598832fe59476b163eb6f3a8ee94c657e
Duncan Skinner <duncanskinner94@gmail.com>
2019-10-03 13:32:33 +0100

    adding Heli as collaborator

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -3,7 +3,15 @@ require 'capybara'
 feature "attack" do
   scenario "attacks player 2 and returns confirmation" do
     sign_in_and_play
-    click_button "Attack Duncan"
+    click_button "Attack"
     expect(page).to have_text("John attacks Duncan!")
+  end
+
+  scenario 'it reduces player 2 hp by 10 points' do
+    sign_in_and_play
+    click_button 'Attack'
+
+    expect(page).to _not have_text("Duncan: 100HP")
+    expect(page).to have_text("Duncan: 90HP")
   end
 end
